10d59f6fb60ca66507c69e87329e1656afe2c671
尚毅力 <14755090+shang-yili@user.noreply.gitee.com>
2024-11-22 11:35:55 +0800

    移除路由

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import router from './router'
+// import router from './router'
 // import BaseData from './views/BaseData.vue'
 // import { createRouter, createWebHistory } from 'vue-router'
 
-// const app = createApp(App)
-const app = createApp({
-  template: '<router-view></router-view>', // 使用 router-view 替代 App
-})
+const app = createApp(App)
+// const app = createApp({
+//   template: '<router-view></router-view>', // 使用 router-view 替代 App
+// })
 
 // // 配置路由
 // const routes = [
@@ -29,8 +29,8 @@ const app = createApp({
 // })
 
 // 使用插件
-app.use(router).mount('#app')
+// app.use(router).mount('#app')
 app.use(ElementPlus)
 
 // 挂载应用
-// app.mount('#app')
+app.mount('#app')
